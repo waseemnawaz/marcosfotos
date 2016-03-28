3d516343d1db3562caba9024dd42f1d5531e9938
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-03-28 13:38:49 +0100

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  #devise_for :users
+  resources :users
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
   get 'static_pages/about'

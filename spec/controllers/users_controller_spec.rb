c9d3d33e6a297e4041d387bcae1985389911c87d
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-03-31 16:30:55 +0100

    modified spec file

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe UsersController, :type => :controller do
+
+  before do
+    @user = User.create!(email: "peter@example.com", password: "1234567890")
+    @user2 = User.create!(email: "maria@no.com", password: "lalalalalala")
+  end
+
+  describe "GET #show" do
+     context "User is logged in" do
+        before do
+          sign_in @user
+        end
+        it "loads correct user details" do
+          get :show, id: @user.id
+          expect(response).to have_http_status(200)
+          expect(assigns(:user)).to eq @user
+        end
+     #end
+
+     it "does not load user two" do
+        get :show, id: @user2.id
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_path)
+      end
+     end
+
+     context "No user is logged in" do
+       it "redirects to login" do
+         get :show, id: @user.id
+         expect(response).to redirect_to(new_user_session_path)
+       end
+     end
+  end
+
+end
